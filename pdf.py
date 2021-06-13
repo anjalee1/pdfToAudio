d668bdfe1de770d0288af273247eea5c9aa27f8f
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2021-06-13 23:28:32 +0530

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -3,14 +3,14 @@ import PyPDF2
 import pyttsx3
 
 # path of the PDF file
-path = open('test.pdf', 'rb')
+path = open('demo.pdf', 'rb')
 
 # creating a PdfFileReader object
 pdfReader = PyPDF2.PdfFileReader(path)
 
 # the page with which you want to start
 # this will read the page of 15th page.
-from_page = pdfReader.getPage(15)
+from_page = pdfReader.getPage(18)
 
 # extracting the text from the PDF
 text = from_page.extractText()
